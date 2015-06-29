750fc6b60ea187721a60c4201a3b87b24750ae31
David Brandt <david+git@kolibri.is>
2015-06-29 14:53:57 +0000

    Fixed bug in date export

--- angular-xlsx.js
+++ angular-xlsx.js
@@ -12,7 +12,7 @@ angular.module("ngXlsx", [])
         }
 
         function datenum(v, date1904) {
-			if(datenumate1904) v+=1462;
+			if(date1904) v+=1462;
 			var epoch = Date.parse(v);
 			return (epoch - new Date(Date.UTC(1899, 11, 30))) / (24 * 60 * 60 * 1000);
 		}
